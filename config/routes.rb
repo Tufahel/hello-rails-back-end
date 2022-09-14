f2ca62ca39fad9999369a8da1483a2a61e5c995e
Tufahel <tufahel96@gmail.com>
2022-09-14 19:43:02 +0600

    routes settings and db:migrate

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api, defaults: {format: 'json'} do 
+    resources :greetings, only:[:index]
+  end
 end
